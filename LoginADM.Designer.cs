95b245a7b6b8a046283d9344e10f7238d30a8f88
lima <igorrmatheus2005@gmail.com>
2026-05-08 22:07:32 -0300

    .........

--- LoginADM.Designer.cs
+++ LoginADM.Designer.cs
@@ -37,46 +37,49 @@
             this.txtUser = new System.Windows.Forms.TextBox();
             this.labelUser = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLista = new System.Windows.Forms.Button();
             this.txtNome = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.txtArea = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.dgvLista = new System.Windows.Forms.DataGridView();
+            this.dtpData = new System.Windows.Forms.DateTimePicker();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
             this.SuspendLayout();
             // 
             // btnConsultar
             // 
-            this.btnConsultar.Location = new System.Drawing.Point(149, 185);
+            this.btnConsultar.Location = new System.Drawing.Point(97, 209);
             this.btnConsultar.Name = "btnConsultar";
             this.btnConsultar.Size = new System.Drawing.Size(75, 23);
             this.btnConsultar.TabIndex = 15;
             this.btnConsultar.Text = "Consultar";
             this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
             // 
             // btnEditar
             // 
-            this.btnEditar.Location = new System.Drawing.Point(234, 153);
+            this.btnEditar.Location = new System.Drawing.Point(224, 180);
             this.btnEditar.Name = "btnEditar";
             this.btnEditar.Size = new System.Drawing.Size(75, 23);
             this.btnEditar.TabIndex = 14;
             this.btnEditar.Text = "Editar";
             this.btnEditar.UseVisualStyleBackColor = true;
-            this.btnEditar.Click += new System.EventHandler(this.buttonRegister_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(149, 153);
+            this.btnCancel.Location = new System.Drawing.Point(139, 180);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 13;
             this.btnCancel.Text = "Cancelar";
             this.btnCancel.UseVisualStyleBackColor = true;
-            this.btnCancel.Click += new System.EventHandler(this.buttonCancel_Click);
             // 
             // btnCadastrar
             // 
-            this.btnCadastrar.Location = new System.Drawing.Point(64, 153);
+            this.btnCadastrar.Location = new System.Drawing.Point(54, 180);
             this.btnCadastrar.Name = "btnCadastrar";
             this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
             this.btnCadastrar.TabIndex = 12;
@@ -116,12 +119,13 @@
             this.labelUser.Size = new System.Drawing.Size(35, 13);
             this.labelUser.TabIndex = 8;
             this.labelUser.Text = "Email:";
-            this.labelUser.Click += new System.EventHandler(this.labelUser_Click);
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.Transparent;
             this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.dtpData);
+            this.groupBox1.Controls.Add(this.btnLista);
             this.groupBox1.Controls.Add(this.txtNome);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.btnConsultar);
@@ -134,12 +138,22 @@
             this.groupBox1.Controls.Add(this.labelPass);
             this.groupBox1.Controls.Add(this.txtUser);
             this.groupBox1.Controls.Add(this.labelUser);
-            this.groupBox1.Location = new System.Drawing.Point(209, 137);
+            this.groupBox1.Location = new System.Drawing.Point(37, 120);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(364, 214);
+            this.groupBox1.Size = new System.Drawing.Size(364, 238);
             this.groupBox1.TabIndex = 16;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = " ";
+            // 
+            // btnLista
+            // 
+            this.btnLista.Location = new System.Drawing.Point(178, 209);
+            this.btnLista.Name = "btnLista";
+            this.btnLista.Size = new System.Drawing.Size(75, 23);
+            this.btnLista.TabIndex = 19;
+            this.btnLista.Text = "Lista";
+            this.btnLista.UseVisualStyleBackColor = true;
+            this.btnLista.Click += new System.EventHandler(this.btnLista_Click);
             // 
             // txtNome
             // 
@@ -177,23 +191,40 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(291, 54);
+            this.label1.Location = new System.Drawing.Point(117, 62);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(220, 55);
             this.label1.TabIndex = 17;
             this.label1.Text = "Cadastro";
+            // 
+            // dgvLista
+            // 
+            this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLista.Location = new System.Drawing.Point(446, 87);
+            this.dgvLista.Name = "dgvLista";
+            this.dgvLista.Size = new System.Drawing.Size(326, 271);
+            this.dgvLista.TabIndex = 18;
+            // 
+            // dtpData
+            // 
+            this.dtpData.Location = new System.Drawing.Point(44, 154);
+            this.dtpData.Name = "dtpData";
+            this.dtpData.Size = new System.Drawing.Size(278, 20);
+            this.dtpData.TabIndex = 20;
             // 
             // LoginADM
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvLista);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
             this.Name = "LoginADM";
             this.Text = "LoginADM";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -215,5 +246,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtArea;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLista;
+        private System.Windows.Forms.DataGridView dgvLista;
+        private System.Windows.Forms.DateTimePicker dtpData;
     }
 }
--- LoginADM.cs
+++ LoginADM.cs
@@ -17,21 +17,28 @@ namespace AtividadePI
         {
             InitializeComponent();
         }
+        const string DADOS_CONEXAO = "server=localhost;user=root;password=;database=db_PI";
 
         private void buttonLogin_Click(object sender, EventArgs e)
         {
             string nome = txtNome.Text;
+            string area = txtArea.Text;
+            string data = dtpData.Text;
+
+            DateTime dataConvertida = DateTime.Parse(data);
+
             int controleLinhasAfetadas = 0;
 
-            string dadosConexao = "server=localhost;user=root;password=;database=db_PI";
-            using (MySqlConnection conn = new MySqlConnection(dadosConexao))
+            using (MySqlConnection conn = new MySqlConnection(DADOS_CONEXAO))
             {
                 conn.Open();
-                string scriptInsert = "INSERT INTO tb_dados (nome) VALUES (@nome)";
+                string scriptInsert = "INSERT INTO tb_dados (nome, area, data) VALUES (@nome, @area, @data)";
 
                 using (MySqlCommand comando = new MySqlCommand(scriptInsert, conn))
                 {
                     comando.Parameters.AddWithValue("@nome", nome);
+                    comando.Parameters.AddWithValue("@area", area);
+                    comando.Parameters.AddWithValue("@data", dataConvertida);
                     controleLinhasAfetadas = comando.ExecuteNonQuery();
                 }
                 conn.Close();
@@ -47,19 +54,51 @@ namespace AtividadePI
             }
         }
 
-        private void buttonCancel_Click(object sender, EventArgs e)
+        private void btnConsultar_Click(object sender, EventArgs e)
         {
+            string idConsulta = txtNome.Text;
 
-        }
+            using (MySqlConnection conn = new MySqlConnection(DADOS_CONEXAO))
+            {
+                conn.Open();
+                string scriptConsultaUnica = "SELECT * FROM tb_dados WHERE id = @id";
+                using (MySqlCommand comando = new MySqlCommand(scriptConsultaUnica, conn))
+                {
+                    comando.Parameters.AddWithValue("@id", idConsulta);
 
-        private void buttonRegister_Click(object sender, EventArgs e)
-        {
+                    var dasdosResultado = comando.ExecuteReader();
+                    while (dasdosResultado.Read())
+                    {
+                        //Caso queira mostrar os dados em um ListBox, por exemplo, você pode fazer algo assim:
+                        //lbResultado.Items.Add(dasdosResultado["id"].ToString());
+                        //lbResultado.Items.Add(dasdosResultado["nome"].ToString());
+                        //lbResultado.Items.Add(dasdosResultado["area"].ToString());
+                        //lbResultado.Items.Add(dasdosResultado["data"].ToString());
+                    }
 
-        }
-
-        private void labelUser_Click(object sender, EventArgs e)
-        {
-
-        }
+                }
+                conn.Close();
+            }
     }
-}
+
+        private void btnLista_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(DADOS_CONEXAO))
+            {
+                conn.Open();
+
+
+                string scriptConsulta = "";
+
+
+
+                using (MySqlCommand comando = new MySqlCommand(scriptConsulta, conn))
+                {
+                    MySqlDataAdapter resultadoConsultaMySql = new MySqlDataAdapter(comando);
+                    DataTable tabelaDados = new DataTable();
+                    resultadoConsultaMySql.Fill(tabelaDados);
+                    dgvLista.DataSource = tabelaDados;
+                }
+                conn.Close();
+            }
+    }
